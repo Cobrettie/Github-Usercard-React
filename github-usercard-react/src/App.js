f952dfcb45b09ee644a3069c11ebf08ecd7e1575
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-09 20:45:50 -0400

    build boilerplate NavMenu component

--- github-usercard-react/src/App.js
+++ github-usercard-react/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { NavMenu } from './Components/NavMenu/NavMenu';
 
 function App() {
   return (
     <div className="App">
+      <NavMenu />
       <h1>Welcome To Your GitHub User Card, Built With React</h1>
     </div>
   );
---
+++ github-usercard-react/src/Components/NavMenu/NavMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NavMenu = () => {
+  return (
+    <div>
+      <nav>
+        <li>UserCard</li>
+      </nav>
+    </div>
+  )
+}
