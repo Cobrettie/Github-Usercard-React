d827e64f9e7e44aa167cd5214c5a77d0d77e5f77
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-09 21:21:54 -0400

    api request to get user followers, success

--- github-usercard-react/src/Components/UserCard/UserCard.js
+++ github-usercard-react/src/Components/UserCard/UserCard.js
@@ -5,7 +5,8 @@ class UserCard extends React.Component {
   constructor() {
     super()
     this.state = {
-      user: {}
+      user: {},
+      followers: []
     }
   }
 
@@ -13,17 +14,30 @@ class UserCard extends React.Component {
     axios
       .get(`https://api.github.com/users/cobrettie`)
       .then(response => {
-        console.log(response)
+        // console.log(response)
         this.setState({user: response.data})
+        const userFollowers = response.data.followers_url
+        return userFollowers
+      })
+      .then(response => {
+        // console.log(response)
+        axios
+          .get(`${response}`)
+          .then(response => {
+            // console.log(response)
+            this.setState({followers: response.data})
+          })
       })
       .catch(err => console.log(err))
   }
 
   render() {
     console.log(this.state.user)
+    console.log(this.state.followers)
     return (
       <div>
         <h2>UserCard Component</h2>
+        <p>{this.state.user.login}</p>
       </div>
     )
   }
