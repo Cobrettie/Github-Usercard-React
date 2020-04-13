5db35b61c84db2f66b61f5326f19bf4accc5f43a
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-12 23:27:49 -0400

    build UserCard component

--- github-usercard-react/src/App.js
+++ github-usercard-react/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React from 'react'
 import axios from 'axios'
-import './App.css';
+import './App.css'
+import UserCard from './Components/UserCard/UserCard'
 
 
 class App extends React.Component {
+  state = {
+    user: {},
+    followers: []
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/Cobrettie')
+      .then(response => {
+        // console.log(response)
+        this.setState({user: response.data})
+      })
+      .catch(err => console.log(err))
+  }
+
+
   render() {
+    console.log('render runnin', this.state.user)
     return(
       <div className='app'>
-
+        <UserCard userData={this.state.user} />
       </div>
     )
   }
--- github-usercard-react/src/Components/UserCard/UserCard.js
+++ github-usercard-react/src/Components/UserCard/UserCard.js
@@ -1,1 +1,24 @@
 import React from 'react'
+
+export default function UserCard(userData) {
+  // console.log('userData', userData)
+  return(
+    <div>
+      <div>
+        <img src={userData.userData.avatar_url} />
+      </div>
+
+      <div>
+        <p>Name: {userData.userData.name}</p>
+        <p>GitHub Handle: {userData.userData.login}</p>
+        <p>Location: {userData.userData.location}</p>
+        <p>Email: {userData.userData.email}</p>
+        <p>Public Repos: {userData.userData.public_repos}</p>
+        <p>Followers: {userData.userData.followers}</p>
+        <p>Following: {userData.userData.following}</p>
+        <p>Bio: {userData.userData.bio}</p>
+        {userData.userData.hireable === true ? <p>Hireable: Yes</p> : <p>Hireable: No</p>}
+      </div>
+    </div>
+  )
+}
