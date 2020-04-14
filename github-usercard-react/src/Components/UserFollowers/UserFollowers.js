830ef68b4ae175b76e96b0a34673f9e922421ae0
Cobrettie <cobrettiegarner3@gmail.com>
2020-04-14 19:43:20 -0400

    style UserFollowers component using styled components

--- github-usercard-react/src/Components/UserFollowers/UserFollowers.js
+++ github-usercard-react/src/Components/UserFollowers/UserFollowers.js
@@ -1,19 +1,23 @@
 import React from 'react'
-
+import { MainHeadingH2, MappedFollowersContainerDiv, FollowerCardDiv, FollowerImg } from './UserFollowersStyles'
 
 export default function UserFollowers(userFollowers) {
   console.log(userFollowers)
   return(
     <div>
-      <h2>Followers</h2>
-      {userFollowers.userFollowers.map(follower => {
-        console.log('follower log', follower)
-        return (
-          <div key={follower.id}>
-            <p>{follower.login}</p>
-          </div>
-        )
-      })}
+      <MainHeadingH2>Followers</MainHeadingH2>
+      <MappedFollowersContainerDiv>
+        {userFollowers.userFollowers.map(follower => {
+          console.log('follower log', follower)
+          return (
+            <FollowerCardDiv key={follower.id}>
+              <FollowerImg src={follower.avatar_url} alt={'Follower\'s Avatar'} />
+              <p>GitHub Handle: {follower.login}</p>
+              <button><a href={`https://github.com/${follower.login}`}>View GitHub Profile</a></button>
+            </FollowerCardDiv>
+          )
+        })}
+      </MappedFollowersContainerDiv>
     </div>
   )
 }
---
+++ github-usercard-react/src/Components/UserFollowers/UserFollowersStyles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const MainHeadingH2 = styled.h2`
+  text-align: center;
+`;
+
+export const MappedFollowersContainerDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+export const FollowerCardDiv = styled.div`
+  width: 20%;
+  border: 1px solid #373737;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 20px;
+  text-align: center;
+`;
+
+export const FollowerImg = styled.img`
+  width: 60%;
+  height: auto;
+`;
